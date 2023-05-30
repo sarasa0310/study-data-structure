14472b9a46b4941afa10be4c6aca845dcb56f82c
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-05-30 12:30:44 +0900

    버블 정렬 다른 구현 방법 추가

--- src/main/java/sorting/bubblesort/BubbleSort.java
+++ src/main/java/sorting/bubblesort/BubbleSort.java
@@ -6,30 +6,45 @@ import java.util.Arrays;
 
 public class BubbleSort {
 
-    public static int[] bubbleSort(int[] input) {
-        boolean isUnsorted = true;
+    public static void standardBubbleSort(int[] input) {
+        int len = input.length;
 
-        while (isUnsorted) {
-            isUnsorted = false;
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = 0; j < len - i - 1; j++) {
+                if (input[j] > input[j + 1]) {
+                    SortingHelper.swap(input, j, j + 1);
+                }
+            }
+        }
+    }
+
+    public static void improvedBubbleSort(int[] input) {
+        boolean swap = true;
+
+        while (swap) {
+            swap = false;
 
             for (int i = 0; i < input.length - 1; i++) {
                 if (input[i] > input[i + 1]) {
                     SortingHelper.swap(input, i, i + 1);
-                    isUnsorted = true;
+                    swap = true;
                 }
             }
         }
-
-        return input;
     }
 
     public static void main(String[] args) {
         int[] test = {1, 8, 3, 6, 25, 76, 33, 92, 12, 55};
+        int[] test2 = {1, 8, 3, 6, 25, 76, 33, 92, 12, 55};
 
         System.out.println(Arrays.toString(test));
+        System.out.println(Arrays.toString(test2));
 
-        bubbleSort(test);
+        improvedBubbleSort(test);
+        standardBubbleSort(test2);
 
         System.out.println(Arrays.toString(test));
+        System.out.println(Arrays.toString(test2));
     }
+
 }
